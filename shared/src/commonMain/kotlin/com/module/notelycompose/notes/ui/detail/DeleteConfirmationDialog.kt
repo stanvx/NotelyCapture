e0bfc753018a9bf36e75218caf673447923c488f
Tosin <tosinmath007@gmail.com>
2025-06-06 19:20:26 +0100

    Modify Delete Dialog

--- shared/src/commonMain/kotlin/com/module/notelycompose/notes/ui/detail/DeleteConfirmationDialog.kt
+++ shared/src/commonMain/kotlin/com/module/notelycompose/notes/ui/detail/DeleteConfirmationDialog.kt
@@ -1,9 +1,13 @@
 package com.module.notelycompose.notes.ui.detail
 
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import com.module.notelycompose.notes.ui.theme.LocalCustomColors
 import notelycompose.shared.generated.resources.Res
 import notelycompose.shared.generated.resources.confirmation
 import notelycompose.shared.generated.resources.confirmation_text
@@ -20,25 +24,48 @@ fun DeleteConfirmationDialog(
     if (showDialog) {
         AlertDialog(
             onDismissRequest = onDismiss,
-            title = { Text(stringResource(Res.string.confirmation)) },
-            text = { Text(stringResource(Res.string.confirmation_text)) },
+            title = {
+                    Text(
+                        text = stringResource(Res.string.confirmation),
+                        color = LocalCustomColors.current.bodyContentColor
+                    )
+            },
+            text = {
+                Text(
+                    text = stringResource(Res.string.confirmation_text),
+                    color = LocalCustomColors.current.bodyContentColor
+                )
+            },
             confirmButton = {
                 TextButton(
                     onClick = {
                         onDismiss()
                         onConfirm()
-                    }
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFC23636),
+                        contentColor = LocalCustomColors.current.bodyContentColor
+                    )
                 ) {
-                    Text(stringResource(Res.string.confirmation_delete))
+                    Text(
+                        text = stringResource(Res.string.confirmation_delete),
+                        color = Color.White
+                    )
                 }
             },
             dismissButton = {
                 TextButton(
                     onClick = onDismiss
                 ) {
-                    Text(stringResource(Res.string.confirmation_cancel))
+                    Text(
+                        text = stringResource(Res.string.confirmation_cancel),
+                        color = LocalCustomColors.current.bodyContentColor
+                    )
                 }
-            }
+            },
+            containerColor = LocalCustomColors.current.shareDialogBackgroundColor,
+            titleContentColor = LocalCustomColors.current.bodyContentColor,
+            shape = RectangleShape
         )
     }
 }
