7647ded9f45fdbc8eba233a91685fc28da2dd439
md-alramlawi <md.alramlawi@gmail.com>
2025-07-04 16:26:54 +0300

    Cleaned-up ContextExtension

--- shared/src/androidMain/kotlin/com/module/notelycompose/extensions/ContextExtension.kt
+++ shared/src/androidMain/kotlin/com/module/notelycompose/extensions/ContextExtension.kt
@@ -1,7 +1,10 @@
 package com.module.notelycompose.extensions
 
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.TileService
 import com.module.notelycompose.MainActivity
 import com.module.notelycompose.service.AudioRecordingService
 
@@ -12,7 +15,6 @@ internal fun Context.startRecordingService(recordingAction: String) {
     startForegroundService(intent)
 }
 
-
 internal fun Context.restartMainActivity() {
     val intent = Intent(this, MainActivity::class.java).apply {
         flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -21,6 +23,32 @@ internal fun Context.restartMainActivity() {
     try {
         startActivity(intent)
     } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+internal fun TileService.startMainActivity(recordingAction: String?) {
+    val intent = Intent(this, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        action = recordingAction
+    }
+    val pendingIntentFlags =
+        PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+
+    val pendingIntent = PendingIntent.getActivity(
+        this,
+        0,
+        intent,
+        pendingIntentFlags
+    )
+
+    try {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startActivityAndCollapse(pendingIntent)
+        } else {
+            startActivity(intent)
+        }
+    } catch (e: PendingIntent.CanceledException) {
         e.printStackTrace()
     }
 }
--- shared/src/androidMain/kotlin/com/module/notelycompose/service/RecordingTileService.kt
+++ shared/src/androidMain/kotlin/com/module/notelycompose/service/RecordingTileService.kt
@@ -1,13 +1,11 @@
 package com.module.notelycompose.service
 
-import android.app.PendingIntent
-import android.content.Intent
 import android.graphics.drawable.Icon
-import android.os.Build
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import com.module.notelycompose.MainActivity
 import com.module.notelycompose.R
+import com.module.notelycompose.extensions.startMainActivity
 import com.module.notelycompose.extensions.startRecordingService
 
 class RecordingTileService : TileService() {
@@ -21,7 +19,7 @@ class RecordingTileService : TileService() {
         if (MainActivity.paused) {
             this.startRecordingService(recordingAction = AudioRecordingService.ACTION_STOP_INSERT)
         } else {
-            startMainActivity(recordingAction = AudioRecordingService.ACTION_STOP_INSERT)
+            this.startMainActivity(recordingAction = AudioRecordingService.ACTION_STOP_INSERT)
         }
         qsTile.state = Tile.STATE_INACTIVE
         qsTile.icon = Icon.createWithResource(this, R.drawable.ic_outline_edit_note)
@@ -38,32 +36,5 @@ class RecordingTileService : TileService() {
             qsTile.icon = Icon.createWithResource(this, R.drawable.ic_outline_edit_note)
         }
         qsTile.updateTile()
-    }
-
-
-    private fun startMainActivity(recordingAction: String?) {
-        val intent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            action = recordingAction
-        }
-        val pendingIntentFlags =
-            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0,
-            intent,
-            pendingIntentFlags
-        )
-
-        try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-                startActivityAndCollapse(pendingIntent)
-            } else {
-                startActivity(intent)
-            }
-        } catch (e: PendingIntent.CanceledException) {
-            e.printStackTrace()
-        }
     }
 }
