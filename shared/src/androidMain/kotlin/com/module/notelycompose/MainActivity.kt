8e4f33f2dd1a7fafaa68eea7aec1606ee6734b74
md-alramlawi <md.alramlawi@gmail.com>
2025-07-05 17:18:29 +0300

    fix: improve activity lifecycle handling

--- shared/src/androidMain/kotlin/com/module/notelycompose/MainActivity.kt
+++ shared/src/androidMain/kotlin/com/module/notelycompose/MainActivity.kt
@@ -1,62 +1,24 @@
 package com.module.notelycompose
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.module.notelycompose.extensions.restartMainActivity
-import com.module.notelycompose.extensions.startRecordingService
-import com.module.notelycompose.service.AudioRecordingService
-import com.module.notelycompose.service.AudioRecordingService.Companion.ACTION_START
-import com.module.notelycompose.service.AudioRecordingService.Companion.ACTION_STOP_INSERT
 import org.koin.android.ext.android.inject
 
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        var paused = false
-    }
     private val permissionLauncherHolder by inject<PermissionLauncherHolder>()
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        permissionLauncherHolder.permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
         installSplashScreen()
+        super.onCreate(savedInstanceState)
+        permissionLauncherHolder.permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
         enableEdgeToEdge()
-        handleIntentAction(intent.action)
         setContent {
             App()
-        }
-    }
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        handleIntentAction(intent.action)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        paused = false
-        if (AudioRecordingService.stopedByTile()) {
-            this.restartMainActivity()
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        paused = true
-    }
-}
-
-private fun Activity.handleIntentAction(intentAction: String?) {
-    when (val action = intentAction) {
-        ACTION_START, ACTION_STOP_INSERT -> {
-            this.startRecordingService(recordingAction = action)
-            intent.setAction(null)
         }
     }
 }
--- shared/src/androidMain/kotlin/com/module/notelycompose/service/RecordingTileService.kt
+++ shared/src/androidMain/kotlin/com/module/notelycompose/service/RecordingTileService.kt
@@ -3,27 +3,22 @@ package com.module.notelycompose.service
 import android.graphics.drawable.Icon
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
-import com.module.notelycompose.MainActivity
 import com.module.notelycompose.R
-import com.module.notelycompose.extensions.startMainActivity
+import com.module.notelycompose.extensions.launchMainActivityFromTile
 import com.module.notelycompose.extensions.startRecordingService
 
 class RecordingTileService : TileService() {
 
     override fun onClick() {
-        if (qsTile.state != Tile.STATE_ACTIVE) {
-            this.startMainActivity(recordingAction = null)
+        if (qsTile.state == Tile.STATE_INACTIVE) {
+            this.launchMainActivityFromTile()
             return
         }
         stopRecording()
     }
 
     private fun stopRecording() {
-        if (MainActivity.paused) {
-            this.startRecordingService(recordingAction = AudioRecordingService.ACTION_STOP_INSERT)
-        } else {
-            this.startMainActivity(recordingAction = AudioRecordingService.ACTION_STOP_INSERT)
-        }
+        this.startRecordingService(recordingAction = AudioRecordingService.ACTION_STOP_FROM_TILE)
         qsTile.state = Tile.STATE_INACTIVE
         qsTile.icon = Icon.createWithResource(this, R.drawable.ic_outline_edit_note)
         qsTile.updateTile()
